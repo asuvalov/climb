295df4c2d0c304267aec94d87ce7cb64c1b9511c
a.suvalov <suvalov.andrew@gmail.com>
2020-07-04 15:28:36 +0300

    added p2pkh

--- climb/src/crypto/base58.hpp
+++ climb/src/crypto/base58.hpp
@@ -13,9 +13,8 @@ std::string encode_base58(const std::vector<char>& data);
 std::vector<char> decode_base58(const char* data, size_t size);
 std::vector<char> decode_base58(const std::string& base58);
 
-std::string encode_base58_check(const unsigned char* data, size_t size);
-std::vector<char> decode_base58_check(const std::string& base58);
-
+std::string encode_base58_check(unsigned char version, const unsigned char* data, size_t size);
+std::pair<unsigned char, std::vector<char>> decode_base58_check(const std::string& base58);
 
 } // crypto
 
--- climb/src/elliptic/public_key.hpp
+++ climb/src/elliptic/public_key.hpp
@@ -2,6 +2,7 @@
 
 #include <vector>
 #include <string>
+#include <cstring>
 
 namespace ecdsa
 {
@@ -9,11 +10,20 @@ namespace ecdsa
 class public_key
 {
 public:
-    public_key(const std::vector<char>& data);
-    public_key(const std::string& hex);
+    explicit public_key(const std::vector<char>& data);
+    explicit public_key(const std::string& hex);
 
     std::vector<char> to_vector() const;
     std::string to_hex() const;
+
+    const char* data() const { return _key.data(); }
+    size_t size() const { return _key.size(); }
+
+    friend bool operator== (const public_key& lhs, const public_key& rhs) {
+        return memcmp(static_cast<const void*>(lhs._key.data()), static_cast<const void*>(rhs._key.data()), lhs.size()) == 0;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const ecdsa::public_key& pubkey) { return os << pubkey.to_hex(); }
 
 private:
     std::vector<char> _key;
